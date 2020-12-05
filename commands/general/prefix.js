db4173960ac666ce5c9de50bfee0298df223e7e4
Ricky Rodrigo <75511393+Riiickyy@users.noreply.github.com>
2020-12-05 15:35:48 +0800

    Refactor code with class "Store"

---
+++ commands/general/prefix.js
@@ -0,0 +1,35 @@
+onst Command = require('../../structures/Command.js');
+const GuildModel = require('../../models/Guild.js');
+
+class Prefix extends Command {
+    constructor(...args) {
+        super(...args);
+    }
+
+    async run(message, args) {
+        if (!message.member.hasPermission('MANAGE_GUILD')) {
+            message.reply(bot.lang.lackOfPermission.random());
+
+            return;
+        }
+
+        let guild = message.guild,
+            prefix = args[0];
+
+        if (args.length > 0) {
+            await GuildModel.updateOne(
+                { id: guild.id },
+                { $set: { prefix: prefix } },
+                { upsert: true }
+            );
+
+            message.channel.send(bot.lang.prefixChanged.format(prefix));
+        } else {
+            let doc = await GuildModel.findOne({ id: guild.id });
+
+            message.channel.send(bot.lang.currentPrefix.format(doc.prefix));
+        }
+    }
+}
+
+module.exports = Prefix;
