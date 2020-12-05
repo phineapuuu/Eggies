3c5aac52c9e33a93f3fe0af45e1d4faf0d7c2480
Ricky Rodrigo <75511393+Riiickyy@users.noreply.github.com>
2020-12-05 15:20:42 +0800

    Create balance.js
    Migrate DB from enmap to mongoDB

---
+++ commands/economy/balance.js
@@ -0,0 +1,17 @@
+
+const Command = require('../../structures/Command.js');
+
+class Balance extends Command {
+    constructor(...args) {
+        super(...args);
+    }
+
+    async run(message) {
+        let member = message.mentions.members.first() || message.member,
+            { money } = await member.info;
+
+        message.channel.send(bot.lang.moneyInPocket.format(member.user.tag, money));
+    }
+}
+
+module.exports = Balance;
